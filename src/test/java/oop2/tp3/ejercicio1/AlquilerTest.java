eb00dedcb5c1b16c771e7425550c49336f1debd0
SoyCharita <mariangelesbaselli@hotmail.com>
2025-04-07 21:46:00 -0300

    El test de Alquiler funciona.

--- src/test/java/oop2/tp3/ejercicio1/AlquilerTest.java
+++ src/test/java/oop2/tp3/ejercicio1/AlquilerTest.java
@@ -1,4 +1,43 @@
 package oop2.tp3.ejercicio1;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AlquilerTest {
+    Libro unLibro;
+    CopiaLibro unaCopia;
+    Cliente unCliente;
+    Alquiler alquilerProgrJava;
+
+    @BeforeEach
+
+    public void incializarVariables(){
+        unLibro = new Libro("Programacion en Java", Libro.REGULARES);
+        unaCopia = new CopiaLibro(unLibro);
+        unCliente = new Cliente("Maria");
+        alquilerProgrJava = new Alquiler(unaCopia, 3);
+        unCliente.alquilar(alquilerProgrJava);
+    }
+
+    @Test
+
+    public void verificarLibro() {
+        assertEquals(0, unLibro.codigoPrecio());
+        assertEquals("Programacion en Java", unLibro.nombre());
+    }
+
+    @Test
+    public void verificarAspectosClientes() {
+        var resultado = unCliente.calcularDeudaYPuntosObtenidos();
+        assertEquals(3.5,resultado[0]);
+        assertEquals(1, resultado[1]);
+    }
+
+    @Test
+    public void verificarAlquiler() {
+        assertEquals(3, alquilerProgrJava.diasAlquilados());
+          }
+
 }
